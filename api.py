655bad1950570d1b92a3adf624858d017edd39d9
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2023-04-13 09:14:47 +0530

    Waiting for the frontend to catchup

--- api.py
+++ api.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=7887,
+        port=5000,
         allow_unsafe_werkzeug=True,
         debug=True,
     )
