438fe6a7e40e92e39157fc6d5dd777c99e6e22b8
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2023-04-14 02:04:01 +0530

    Api port changed

--- api.py
+++ api.py
@@ -238,7 +238,7 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=5000,
+        port=7887,
         allow_unsafe_werkzeug=True,
         debug=True,
     )
